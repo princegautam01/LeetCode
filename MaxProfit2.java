8c778e14f441e9ca4bc4328cec448a268bc488dd
Prince <princegautam133@gmail.com>
2025-01-31 22:34:37 +0530

    Upload

---
+++ MaxProfit2.java
@@ -0,0 +1,13 @@
+public class MaxProfit2 {
+    public int maxProfit(int[] prices) {
+        int start = prices[0];
+        int max = 0;
+        for(int i = 1;i<prices.length;i++){
+            if(start<prices[i]){
+                max+= prices[i]-start;
+            }
+            start = prices[i];
+        }
+        return max;
+    }
+}
